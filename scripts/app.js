86ea71dac64783547d0efdf7b1692f66af50e44d
wani <waynekhalifa.wk@gmail.com>
2019-07-08 10:02:12 +0100

    removes reveal on scroll behaviour and styles the actions button in mobile view

--- scripts/app.js
+++ scripts/app.js
@@ -3,16 +3,15 @@ import OwlSliders from './modules/OwlSliders';
 import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/RevealOnScroll';
 import Modal from './modules/Modal';
-import GoogleMap from './modules/GoogleMap';
 
 $(document).ready(function(){
     new MobileMenu();
     new OwlSliders();
     new StickyHeader();
-    new RevealOnScroll( $('.about'), '85%', 'left' );
-    new RevealOnScroll( $('.about-us .entry-content'), '50%', 'left' );
-    new RevealOnScroll( $('.testimonials'), '85%', 'right' );
-    new RevealOnScroll( $('.company-contact'), '65%', 'zoom-out' );
+    // new RevealOnScroll( $('.about'), '85%', 'left' );
+    // new RevealOnScroll( $('.about-us .entry-content'), '50%', 'left' );
+    // new RevealOnScroll( $('.testimonials'), '85%', 'right' );
+    // new RevealOnScroll( $('.company-contact'), '65%', 'zoom-out' );
     new Modal( $('.request-modal'), $('.request-callback'), $('.backdrop') );
     new Modal( $('.refer-modal'), $('.refer-friend'), $('.backdrop') );
     new Modal( $('.send-cv-modal'), $('.send-cv'), $('.backdrop') );
