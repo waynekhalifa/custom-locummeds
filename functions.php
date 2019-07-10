3628466610295f40bc53548e5f3dde31dd2b7d81
waynekhalifa.wk@gmail.com <waynekhalifa.wk@gmail.com>
2019-07-10 17:09:23 +0100

    removes the api discovery from the site

--- functions.php
+++ functions.php
@@ -169,3 +169,16 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Remove the api discovery from the site
+ */
+
+// Disable REST API link tag
+remove_action('wp_head', 'rest_output_link_wp_head', 10);
+
+// Disable oEmbed Discovery Links
+remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+
+// Disable REST API link in HTTP headers
+remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+
