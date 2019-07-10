0b5302b33477246b7eade1ab0a8c2494808cdf50
Wayne Khalifa <wani@start-solutions.net>
2019-07-10 02:13:41 +0200

    styles download candidate sheet button

--- page-candidate.php
+++ page-candidate.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 
 <div id="primary" class="content-area">
-	<main id="main" class="site-main">
+	<main id="main" class="site-main candidate">
 		
         <?php while ( have_posts() ) : the_post(); ?>
             <div class="content-wrap">
