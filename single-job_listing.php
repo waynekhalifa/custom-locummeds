c5279099dee53184ba60dfbb63f1d7bc7aaebec3
waynekhalifa.wk@gmail.com <waynekhalifa.wk@gmail.com>
2019-07-22 13:34:55 +0100

    adds sharing links to sharing buttons

--- single-job_listing.php
+++ single-job_listing.php
@@ -89,10 +89,11 @@ get_header();
 
                                 <div class="more-info"><a href="tel:01923594002"><?php _e('Call For More Information', 'custom-locummeds'); ?></a></div>
 
+                                <?php $post = "https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"; ?>
                                 <ul class="social-sharings">
-                                    <li><a href="#" target="_blank" rel="nofollow" class="facebook"><?php _e('Share', 'custom-locummeds'); ?></a></li>
-                                    <li><a href="#" target="_blank" rel="nofollow" class="twitter"><?php _e('Tweet', 'custom-locummeds'); ?></a></li>
-                                    <li><a href="#" target="_blank" rel="nofollow" class="google"><?php _e('Google', 'custom-locummeds'); ?></a></li>
+                                    <li><a href="https://www.facebook.com/sharer.php?u=<?php echo $post; ?>" target="_blank" rel="nofollow" class="facebook"><?php _e('Share', 'custom-locummeds'); ?></a></li>
+                                    <li><a href="https://twitter.com/share?url=<?php echo $post; ?>" target="_blank" rel="nofollow" class="twitter"><?php _e('Tweet', 'custom-locummeds'); ?></a></li>
+                                    <li><a href="https://plus.google.com/share?url=<?php echo $post; ?>" target="_blank" rel="nofollow" class="google"><?php _e('Google', 'custom-locummeds'); ?></a></li>
                                 </ul>
                             </div>
                         </div><!-- .entry-content -->
